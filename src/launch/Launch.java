d192599003f9430104e6979192e97c2607f3a1c4
Justin Rivera <jrivera2@umw.edu>
2016-12-08 00:53:55 -0500

    All buttons placed

--- src/launch/Launch.java
+++ src/launch/Launch.java
@@ -13,13 +13,21 @@ import javax.swing.border.*;
  * @author jariv
  */
 public class Launch extends JPanel implements ActionListener {
-    
+
     JLabel picture;
-    JPanel butt;
-    
+    JPanel rea;
+    JPanel rand;
+    JPanel cust;
+    JTabbedPane missileSet;
+    JPanel custIn;
+    JLabel rangeLabel = new JLabel("Range (km)");
+    JTextField range = new JTextField("0");
+    JLabel blastLabel = new JLabel("Warhead Size (kt)");
+    JTextField blast = new JTextField("0");
+
     Launch() {
         super(new BorderLayout());
-        
+
         String[] missileStrings = {"Big", "Small", "Tiny", "Giant", "You"};
         String[] countryStrings = {"USA", "Russia", "France", "Cuba", "Italy"};
 
@@ -32,15 +40,46 @@ public class Launch extends JPanel implements ActionListener {
         JComboBox missileList = new JComboBox(missileStrings);
         missileList.setSelectedIndex(0);
         missileList.addActionListener(this);
-        
+
+        //Fire button
         JButton fire = new JButton();
         fire.setText("FIRE");
         fire.setBackground(Color.red);
         fire.setForeground(Color.white);
-        fire.setFont(new Font("Serif",Font.BOLD,24));
+        fire.setFont(new Font("Serif", Font.BOLD, 24));
         //fire.setBorder(new LineBoarder());
-        //Set combo box set
-        butt = new JPanel();
+
+        //Set random tab
+        rand = new JPanel();
+        JButton rdm = new JButton();
+        rdm.setText("Random Missile");
+        rand.add(rdm, BorderLayout.CENTER);
+
+        //Set combo box tab
+        rea = new JPanel();
+        rea.add(countryList, BorderLayout.WEST);
+        rea.add(missileList, BorderLayout.EAST);
+        JButton setA = new JButton();
+        setA.setText("Set");
+        rea.add(setA, BorderLayout.LINE_END);
+        
+        //Set input for custom missile
+        custIn = new JPanel();
+        custIn.add(rangeLabel);
+        custIn.add(range);
+        custIn.add(blastLabel);
+        custIn.add(blast);
+        
+        //Set custom tab
+        cust = new JPanel();
+        JLabel test = new JLabel();
+        test.setText("Please enter the following values:");
+        cust.add(test, BorderLayout.LINE_START);
+        cust.add(custIn, BorderLayout.CENTER);
+        JButton setC = new JButton();
+        setC.setText("Set");
+        cust.add(setC, BorderLayout.LINE_END);
+
         //Set up the picture.
         picture = new JLabel();
         picture.setFont(picture.getFont().deriveFont(Font.ITALIC));
@@ -54,14 +93,17 @@ public class Launch extends JPanel implements ActionListener {
         } catch (Exception e) {
         }
         picture.setIcon(new ImageIcon(image));
-        
+
         picture.setPreferredSize(new Dimension(1544, 777 + 10));
-        
-        butt.setLayout(new FlowLayout());
-        butt.add(countryList);
-        butt.add(missileList);
-        
-        add(butt, BorderLayout.PAGE_START);
+
+        //Add tabs
+        missileSet = new JTabbedPane();
+        missileSet.addTab("Random Missile", rand);
+        missileSet.addTab("Real Missile", rea);
+        missileSet.addTab("Custom Missile", cust);
+
+        //Fill page
+        add(missileSet, BorderLayout.PAGE_START);
         add(picture, BorderLayout.CENTER);
         add(fire, BorderLayout.PAGE_END);
         setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
@@ -97,7 +139,7 @@ public class Launch extends JPanel implements ActionListener {
         frame.pack();
         frame.setVisible(true);
     }
-    
+
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
