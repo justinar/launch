bfda53faecb5466f20a541b7d11479d235dc2727
Justin Rivera <jrivera2@umw.edu>
2016-12-08 20:21:55 -0500

    Full display, bug with country combobox

--- src/launch/Launch.java
+++ src/launch/Launch.java
@@ -1,12 +1,12 @@
 package launch;
 
+import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
 import javax.swing.*;
 import java.io.*;
 import javax.imageio.*;
-import javax.swing.border.*;
 
 /**
  *
@@ -18,28 +18,42 @@ public class Launch extends JPanel implements ActionListener {
     JPanel rea;
     JPanel rand;
     JPanel cust;
+    JPanel rest;
     JTabbedPane missileSet;
     JPanel custIn;
     JLabel rangeLabel = new JLabel("Range (km)");
     JTextField range = new JTextField("0");
     JLabel blastLabel = new JLabel("Warhead Size (kt)");
     JTextField blast = new JTextField("0");
+    JComboBox countryList;
+    JComboBox missileList;
+    DefaultComboBoxModel model;
+    String[] countryStrings;
+    //String[] missileStrings = new String;
+    double activeRange;
+    double activeBlast;
 
     Launch() {
         super(new BorderLayout());
 
-        String[] missileStrings = {"Big", "Small", "Tiny", "Giant", "You"};
-        String[] countryStrings = {"USA", "Russia", "France", "Cuba", "Italy"};
+        //Initialize class variables
+        activeRange = 0.0;
+        activeBlast = 0.0;
+        //Create lists
+        countryStrings = arrayFromFile("countries");
+        //missileStrings = {"No","Yes"};
 
         //Create the combo box for countries
-        JComboBox countryList = new JComboBox(countryStrings);
+        countryList = new JComboBox(countryStrings);
         countryList.setSelectedIndex(0);
+        countryList.setActionCommand("comboBoxChanged");
         countryList.addActionListener(this);
 
         //Create the combo box for missiles
-        JComboBox missileList = new JComboBox(missileStrings);
+        missileList = new JComboBox(new String[1]);
+        model = new DefaultComboBoxModel(new String[1]);
+        missileList.setModel(model);
         missileList.setSelectedIndex(0);
-        missileList.addActionListener(this);
 
         //Fire button
         JButton fire = new JButton();
@@ -47,13 +61,26 @@ public class Launch extends JPanel implements ActionListener {
         fire.setBackground(Color.red);
         fire.setForeground(Color.white);
         fire.setFont(new Font("Serif", Font.BOLD, 24));
+        fire.setActionCommand("fire");
+        fire.addActionListener(this);
         //fire.setBorder(new LineBoarder());
 
         //Set random tab
         rand = new JPanel();
         JButton rdm = new JButton();
         rdm.setText("Random Missile");
+        rdm.setActionCommand("random");
+        rdm.addActionListener(this);
         rand.add(rdm, BorderLayout.CENTER);
+
+        //Set reset tab
+        rest = new JPanel();
+        JButton rst = new JButton();
+        rst.setText("Random Missile");
+        rst.setBackground(Color.green);
+        rst.setActionCommand("reset");
+        rst.addActionListener(this);
+        rest.add(rst, BorderLayout.CENTER);
 
         //Set combo box tab
         rea = new JPanel();
@@ -61,15 +88,17 @@ public class Launch extends JPanel implements ActionListener {
         rea.add(missileList, BorderLayout.EAST);
         JButton setA = new JButton();
         setA.setText("Set");
+        setA.setActionCommand("set");
+        setA.addActionListener(this);
         rea.add(setA, BorderLayout.LINE_END);
-        
+
         //Set input for custom missile
         custIn = new JPanel();
         custIn.add(rangeLabel);
         custIn.add(range);
         custIn.add(blastLabel);
         custIn.add(blast);
-        
+
         //Set custom tab
         cust = new JPanel();
         JLabel test = new JLabel();
@@ -78,6 +107,8 @@ public class Launch extends JPanel implements ActionListener {
         cust.add(custIn, BorderLayout.CENTER);
         JButton setC = new JButton();
         setC.setText("Set");
+        setC.setActionCommand("set");
+        setC.addActionListener(this);
         cust.add(setC, BorderLayout.LINE_END);
 
         //Set up the picture.
@@ -101,6 +132,7 @@ public class Launch extends JPanel implements ActionListener {
         missileSet.addTab("Random Missile", rand);
         missileSet.addTab("Real Missile", rea);
         missileSet.addTab("Custom Missile", cust);
+        missileSet.addTab("Reset Sim", rest);
 
         //Fill page
         add(missileSet, BorderLayout.PAGE_START);
@@ -109,12 +141,21 @@ public class Launch extends JPanel implements ActionListener {
         setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
     }
 
-    /**
-     * Listens to the combo box.
-     */
+    //Listener
     public void actionPerformed(ActionEvent e) {
-        JComboBox cb = (JComboBox) e.getSource();
-        String missile = (String) cb.getSelectedItem();
+        String act = (String) e.getActionCommand();
+
+        if (act.equals("fire")) {
+        } else if (act.equals("set")) {
+        } else if (act.equals("random")) {
+        } else if (act.equals("reset")) {
+        } else {
+            JOptionPane.showMessageDialog(null, countryList.getSelectedIndex(), "Check", 2);
+            String country = countryStrings[countryList.getSelectedIndex()];
+            //String[] missileStrings = arrayFromFile("United States");
+            model = new DefaultComboBoxModel(arrayFromFile(countryList.getSelectedItem().toString().trim()));
+            missileList.setModel(model);
+        }
     }
 
     /**
@@ -140,7 +181,31 @@ public class Launch extends JPanel implements ActionListener {
         frame.setVisible(true);
     }
 
+    static String[] arrayFromFile(String who) {
+        ArrayList<String> pull = new ArrayList<>();
+        Scanner in = null;
+        try {
+            in = new Scanner(new File("./data/" + who + ".csv"));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e, "Error: File not found", 0);
+            System.out.println("bad file name, check structure");
+            System.exit(1);
+        }
+        in.useDelimiter(",");
+        while (in.hasNext()) {
+            pull.add(in.next());
+        }
+        return pull.toArray(new String[pull.size()]);
+    }
+
     public static void main(String[] args) {
+        /*
+        **As per suggestion from Oracle's tutorial, GUI is initialized in 
+        **constructor allowing dynamic alterations without crashing/hanging
+        **
+        **Main is used to start the constructor and get the ball rolling
+        **The entire program is run by calling various functions as needed
+         */
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
